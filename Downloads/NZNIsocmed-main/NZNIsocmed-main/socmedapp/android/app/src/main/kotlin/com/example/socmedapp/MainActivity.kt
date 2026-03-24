1b036d85b90a67c517fbedc1226afa25a416caf1
tannnxxx <thristanjaybarcebal22@gmail.com>
2026-03-24 16:55:22 +0800

    Delete Downloads/NZNIsocmed-main/NZNIsocmed-main directory

--- Downloads/NZNIsocmed-main/NZNIsocmed-main/socmedapp/android/app/src/main/kotlin/com/example/socmedapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.socmedapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Downloads/NZNIsocmed-main/NZNIsocmed-main/socmedapp/lib/models/user_model.dart
+++
@@ -1,8 +0,0 @@
-class UserModel {
-
-  final String email;
-
-  UserModel({
-    required this.email,
-  });
-}
